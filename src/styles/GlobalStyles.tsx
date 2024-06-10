760c3c25ed9af403e714ada6d6cdc03758e5c8eb
Henrique <henriquepinheiroxavier@gmail.com>
2024-06-10 16:24:56 -0300

    initial repository - vite+react+typescrit

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    font-size: 100%;
+    font: inherit;
+    scroll-behavior: smooth;
+    vertical-align: baseline;
+    list-style: none;
+    text-decoration: none;
+    line-height: 1.5;
+  }   
+`;
+
+export default GlobalStyles;
